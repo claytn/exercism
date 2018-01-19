25d340a559a7259744cbf44c1a877e8499d8c0e6
marsha88 <marsha88@purdue.edu>
2018-01-19 15:42:19 -0500

    completed bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,18 +1,17 @@
 module Bob (responseFor) where
-{-- Test cases pass. Clean up needs to be done. --}
 
 import Data.Char (isSpace, isAlpha, isUpper)
 
 isQuestion :: String -> Bool
 isQuestion xs
-    | null trimmedString = False
-    | last trimmedString == '?' = True
-    | otherwise = False
-    where trimmedString = [x | x <- xs, not $ isSpace x]
+    | null xs           = False
+    | last xs == '?'    = True
+    | isSpace $ last xs = isQuestion $ init xs
+    | otherwise         = False
 
-isExclamation :: String -> Bool
-isExclamation xs
-    | null alphaList && null upperAlphaList = False
+isYelling :: String -> Bool
+isYelling xs
+    | null alphaList || null upperAlphaList = False
     | alphaList == upperAlphaList           = True
     | otherwise                             = False
     where alphaList      = [x | x <- xs, (isAlpha x)]
@@ -23,11 +22,11 @@ isNothing xs = null (filter (\c -> not $ isSpace c) xs)
 
 responseFor :: String -> String
 responseFor xs
-  | question && exclamation = "Calm down, I know what I'm doing!"
-  | question                = "Sure."
-  | exclamation             = "Whoa, chill out!"
-  | nothing                 = "Fine. Be that way!"
-  | otherwise               = "Whatever."
+  | question && yelling = "Calm down, I know what I'm doing!"
+  | question            = "Sure."
+  | yelling             = "Whoa, chill out!"
+  | nothing             = "Fine. Be that way!"
+  | otherwise           = "Whatever."
   where question    = isQuestion xs
-        exclamation = isExclamation xs
+        yelling     = isYelling xs
         nothing     = isNothing xs
